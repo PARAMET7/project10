f385baf463f45d1a8aeb08e9522f89a16b227cb3
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-12-01 16:51:29 +0100

    paginatin and anonymous cart

--- nextjs-ecommerce/src/app/add-product/page.tsx
+++ nextjs-ecommerce/src/app/add-product/page.tsx
@@ -29,6 +29,16 @@ async function addProduct(formData: FormData) {
   }
 
   await prisma.product.create({
+        data: {name, description, imageUrl, price},
+      });
+
+  // for (let i=0; i<50; i++ ){
+  //   await prisma.product.create({
+  //     data: {name, description, imageUrl, price}
+  //   });
+  // }
+
+  await prisma.product.create({
     data: {name, description, imageUrl, price}
   });
 
--- nextjs-ecommerce/src/app/page.tsx
+++ nextjs-ecommerce/src/app/page.tsx
@@ -1,15 +1,34 @@
+import PaginationBar from "@/components/PaginationBar";
 import ProductCard from "@/components/ProductCard";
 import { prisma } from '@/lib/db/prisma';
 import Image from 'next/image';
 import Link from "next/link";
 
-export default async function Home() {
+
+interface HomeProps {
+  searchParams: {page:string};
+}
+
+export default async function Home({searchParams:{page ="1"}}: HomeProps) {
+  const currentPage = parseInt(page);
+
+  const pageSize =6;
+  const heroItemcount = 1;
+
+
+  const totalItemCount = await prisma.product.count();
+
+  const totalPages= Math.ceil((totalItemCount - heroItemcount)/pageSize)
+
+
   try {
     // Fetch products from the database
     const products = await prisma.product.findMany({
       orderBy: {
         id: 'desc',  // Order by descending 'id' if 'id' exists in your schema
       },
+      skip: (currentPage-1) * pageSize + (currentPage === 1? 0: heroItemcount),
+      take: pageSize + (currentPage === 1? heroItemcount : 0),
     });
 
     // Check if products are available
@@ -19,7 +38,8 @@ export default async function Home() {
 
     // Render the first product from the list
     return (
-      <div>
+      <div className="flex flex-col items-center-center">
+        {currentPage===1 && (
         <div className="hero rounded-xl bg-base-200">
           <div className="hero-content flex-col lg:flex-row">
             <Image
@@ -40,11 +60,15 @@ export default async function Home() {
               </div>
           </div>
         </div>
+        )}
         <div className="my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-          {products.slice(1).map(product => (
+          {(currentPage === 1? products.slice(1): products).map(product => (
             <ProductCard product={product} key={product.id}/>
           ))}
         </div>
+        {totalPages > 1 &&
+        (<PaginationBar currentPage={currentPage} totalPages={totalPages}/>)
+        }
       </div>
     );
   } catch (error) {
